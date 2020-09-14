86d4b11cea1c84b3625ae7f19d14c5c8e562e99d
Igi Espiritu <igi@candybanners.com>
2020-09-14 10:06:52 +0800

    added vuikit

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,8 +1,15 @@
 <template>
-  <div id="hero">
-    <div class="pic-container">
-      <img src="../assets/images/pic.jpg" alt="">
+  <div id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="col"></div>
+    <div class="col">
+      <div class="pic-container">
+        <img src="../assets/images/pic.jpg" alt="">
+      </div>
     </div>
+    <div class="col">
+      <h3><vk-icons-code></vk-icons-code> Miguel Joseph Espiritu</h3>
+    </div>
+    <div class="col"></div>
   </div>
 </template>
 
@@ -18,15 +25,17 @@ export default {
   justify-content: center;
   align-items: center;
   height: 70vh;
-  background-color: #000;
+  background-color: #4d4d4d;
 
   .pic-container {
     width: 20vw;
     height: 20vw;
     border-radius: 50%;
     overflow: hidden;
+    margin: auto;
 
     img {
+      max-width: unset;
       height: 100%;
     }
   }
--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vuikit from 'vuikit'
+import VuikitIcons from '@vuikit/icons'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(Vuikit)
+Vue.use(VuikitIcons)
 
 import './custom.scss'
+import '@vuikit/theme'
 
 new Vue({
   render: h => h(App),
